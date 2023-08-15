6b4e13f367cecc48d474fc8df58f6104314afb1a
swarnaIITDU <swarna-12-2019116857@it.du.ac.bd>
2023-08-15 23:47:41 +0600

    Basic UI

---
+++ chessAi/chessEngine.py
@@ -0,0 +1,10 @@
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR", "bB", "bK", "bQ", "bN"],
+            ["bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wB", "wK", "wQ", "wN"]
+        ]
---
+++ chessAi/chessMain.py
@@ -0,0 +1,57 @@
+import pygame as p
+from chessAi import chessEngine
+
+width=320
+height=384
+xDimension=5
+yDimension=6
+
+squareSize=64
+maxFPS=15
+
+images={}
+
+def loadImages():
+    pieces=['bR','bB','bK','bQ','bN','bp','wR','wB','wK','wQ','wN','wp']
+    for piece in pieces:
+        images[piece]=p.transform.scale( p.image.load('images/'+piece+'.png'),(squareSize,squareSize))
+
+def main():
+    p.init()
+    screen=p.display.set_mode((width,height))
+    clock=p.time.Clock()
+    screen.fill(p.Color("black"))
+
+    gameState=chessEngine.GameState()
+
+    loadImages()
+    running=True
+    while running:
+        for e in p.event.get():
+            if e.type==p.QUIT:
+                running=False
+        drawGameState(screen, gameState)
+        clock.tick(maxFPS)
+        p.display.flip()
+
+def drawGameState(screen,gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colors=[p.Color("white"), p.Color("gray")]
+    for r in range(yDimension):
+        for c in range(xDimension):
+            color=colors[((r+c)%2)]
+            p.draw.rect(screen,color,p.Rect(c*squareSize, r*squareSize, squareSize, squareSize))
+
+
+def drawPieces(screen,board):
+    for r in range(yDimension):
+        for c in range(xDimension):
+            piece=board[r][c]
+            if piece != "--":
+                screen.blit(images[piece],p.Rect(c*squareSize, r*squareSize, squareSize, squareSize))
+
+if __name__=='__main__':
+    main()
